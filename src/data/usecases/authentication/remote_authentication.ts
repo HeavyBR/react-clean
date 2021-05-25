509c1195323a85407f57ffaf1ab0e1ef2921dae2
Matheus Cumpian <matheus.cumpian@sensedia.com>
2021-05-25 18:42:04 -0300

    feat: ensure that the remote authentication returns the response body of HttpPostClient on ok status

--- src/data/usecases/authentication/remote_authentication.ts
+++ src/data/usecases/authentication/remote_authentication.ts
@@ -3,20 +3,20 @@ import { HttpStatusCode } from '@/data/protocols/http/http-response'
 import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-error'
 import { UnexpectedError } from '@/domain/errors/unexpected-error'
 import { AccountModel } from '@/domain/models/account'
-import { AuthenticationParams } from '@/domain/usecases/authentication'
+import { Authentication, AuthenticationParams } from '@/domain/usecases/authentication'
 
-export class RemoteAuthentication {
+export class RemoteAuthentication implements Authentication {
   constructor (
     private readonly url: string,
     private readonly httpPostClient: HttpPostClient<AuthenticationParams, AccountModel>
   ) { }
 
-  async auth (params: AuthenticationParams): Promise<void> {
+  async auth (params: AuthenticationParams): Promise<AccountModel> {
     const response = await this.httpPostClient.post({ url: this.url, body: params })
 
     switch (response.statusCode) {
       case HttpStatusCode.unauthorized: throw new InvalidCredentialsError()
-      case HttpStatusCode.ok: return await Promise.resolve()
+      case HttpStatusCode.ok: return response.body as AccountModel
       default: throw new UnexpectedError()
     }
   }
